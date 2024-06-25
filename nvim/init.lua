7982ec83c9323ca06b86c2553c147a3c6effd9b9
Carlos Delgado Ramos <carlosdelgadoramos2002@gmail.com>
2024-06-25 11:42:10 +0200

    Added a keymap to fuzzy find files that are included by Git

--- nvim/init.lua
+++ nvim/init.lua
@@ -135,6 +135,7 @@ vim.keymap.set('v', '<a-k>', ':m \'<-2<cr>gv=gv', silent)
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fF', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fG', builtin.grep_string, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
