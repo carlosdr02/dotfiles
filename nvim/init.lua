04aad809e57f4c94f3ef8c116a18a5a9f4da54c8
Carlos Delgado Ramos <carlosdelgadoramos2002@gmail.com>
2024-06-26 16:31:13 +0200

    Put the numeric harpoon keymappings into a for loop

--- nvim/init.lua
+++ nvim/init.lua
@@ -158,15 +158,10 @@ local ui = require('harpoon.ui')
 vim.keymap.set('n', '<leader>a', mark.add_file)
 vim.keymap.set('n', '<leader>m', ui.toggle_quick_menu)
 
-vim.keymap.set('n', '<a-1>', function() ui.nav_file(1) end)
-vim.keymap.set('n', '<a-2>', function() ui.nav_file(2) end)
-vim.keymap.set('n', '<a-3>', function() ui.nav_file(3) end)
-vim.keymap.set('n', '<a-4>', function() ui.nav_file(4) end)
-vim.keymap.set('n', '<a-5>', function() ui.nav_file(5) end)
-vim.keymap.set('n', '<a-6>', function() ui.nav_file(6) end)
-vim.keymap.set('n', '<a-7>', function() ui.nav_file(7) end)
-vim.keymap.set('n', '<a-8>', function() ui.nav_file(8) end)
-vim.keymap.set('n', '<a-9>', function() ui.nav_file(9) end)
+for i = 1, 9 do
+    local lhs = '<a-' .. i .. '>'
+    vim.keymap.set('n', lhs, function() ui.nav_file(i) end)
+end
 
 vim.keymap.set('n', '<c-p>', vim.diagnostic.goto_prev)
 vim.keymap.set('n', '<c-n>', vim.diagnostic.goto_next)
