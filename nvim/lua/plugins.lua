8bd7f94d7160bc1624958d1942d7462e63dc6e3f
Carlos Delgado Ramos <carlosdelgadoramos2002@gmail.com>
2024-07-01 23:59:17 +0200

    Added the lua language server

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -93,7 +93,7 @@ lspconfig.clangd.setup {
     cmd = { 'clangd', '--header-insertion=never' },
     capabilities = capabilities,
     on_attach = function()
-        vim.keymap.set('n', '<leader>s', '<cmd>ClangdSwitchSourceHeader<cr>', opts)
+        vim.keymap.set('n', '<leader>s', '<cmd>ClangdSwitchSourceHeader<cr>')
     end
 }
 
@@ -103,6 +103,31 @@ for _, lsp in ipairs(servers) do
         capabilities = capabilities
     }
 end
+
+require'lspconfig'.lua_ls.setup {
+    on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+            return
+        end
+
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+                version = 'LuaJIT'
+            },
+            workspace = {
+                checkThirdParty = false,
+                library = {
+                    vim.env.VIMRUNTIME,
+                    "${3rd}/luv/library"
+                }
+            }
+        })
+    end,
+    settings = {
+        Lua = {}
+    }
+}
 
 local luasnip = require 'luasnip'
 
